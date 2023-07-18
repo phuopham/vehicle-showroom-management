932c84280901051ebb3637fbd99d13be3862c1f8
phuopham <108047552+phuopham@users.noreply.github.com>
2023-07-18 11:25:35 +0700

    init project

---
+++ src/app/layout.tsx
@@ -0,0 +1,31 @@
+'use client'
+
+import { Navbar } from '@/components/navbar'
+import './globals.css'
+import type { Metadata } from 'next'
+import { Inter } from 'next/font/google'
+import { ThemeProvider } from 'next-themes'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export const metadata: Metadata = {
+  title: 'Vehicle Showroom management',
+  description: 'Group 1 - 3rd project at APTECH',
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>
+        <ThemeProvider attribute='class' defaultTheme='system' enableSystem>
+          <Navbar />
+          {children}
+        </ThemeProvider>
+      </body>
+    </html>
+  )
+}
